213887ffb5e97d6cd123c7537e2225cadc9c6ecb
Jordan Ahn <jordan.ahn@gmail.com>
2021-11-04 13:54:58 -0400

    changed colors and updated renv.lock

--- R/barchart.R
+++ R/barchart.R
@@ -1,8 +1,8 @@
 here::i_am("R/barchart.R")
 data(mtcars)
 
-library(RColorBrewer)
-colors <- brewer.pal(3, "Dark2")
+colors <- c('red','blue','green')
+
 
 tab <- table(mtcars$cyl, mtcars$gear)
 x_names <- paste0("Type ", 1:3)
